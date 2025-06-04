b7ad36544e155bce2b462544549a05a3482f3c8d
adnv3k <adnv3k@gmail.com>
2025-06-04 11:00:01 -0700

    Change add_entry shortcut back to Shift + Enter
    - Change focus to be set on set_number_of_images with the text highlighted after append_schedule() is called
    - Change tooltip of button to match

--- main_window.py
+++ main_window.py
@@ -1232,10 +1232,10 @@ class Ui_MainWindow(object):
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'Nanum Gothic\'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:13pt; color:#000000;\">Add entry to the schedule below</span></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:13pt; font-weight:550; color:#000000;\">Ctrl + A</span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:13pt; font-weight:550; color:#000000;\">Shift + Enter</span></p>\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
         self.add_entry.setText(_translate("MainWindow", "Add"))
-        self.add_entry.setShortcut(_translate("MainWindow", "Ctrl+A"))
+        self.add_entry.setShortcut(_translate("MainWindow", "Shift+Enter"))
         self.label_8.setToolTip(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
